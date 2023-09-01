dfa19b1967ae5983afcfd1436a948039df8e3e43
habinnn <gkqls7423@gmail.com>
2023-09-01 14:02:28 +0900

    함수 문제 풀이

--- day3/exam3.java
+++ day3/exam3.java
@@ -1,46 +1,60 @@
 package day3;
 
 public class exam3 {
+    public static void greeting1(int num){
 
-    public static String introduce(int num){
-        for(int num = 0; num < 10; num++) {
-            String result = "안녕하세요. 저는 20살 홍길동입니다.!";
-            System.out.println(result);
-            return result;
+        if(num == 2){
+            System.out.println("하이");
+        } else if (num == 3) {
+            System.out.println("봉쥬");
+        }else{
+            System.out.println("안녕하세요");
+        }
+    }
+    public static void greeting2( ,num1){
+        for(int i = 0; i < num1; i++){
+            System.out.println(greeting1(),num1);
         }
 
 
-
-
     }
-
-
     public static void main(String[] args) {
-        introduce(3);
+
+        // 매개변수는 인사말 언어를 의미
+        // 1은 한국어, 2는 영어, 3은 프랑스어
+
+        greeting1(2);
+        // 하이~
+
+        greeting1(3);
+        // 봉쥬
+
+        greeting1(1);
+        // 안녕하세요
 
 
-        // 안녕하세요. 20살 홍길동입니다.!
-        // 안녕하세요. 20살 홍길동입니다.!
-        // 안녕하세요. 20살 홍길동입니다.!
+        // 첫번째는 언어, 두번째는 횟수를 의미
 
-        introduce(5);
-        // 안녕하세요. 20살 홍길동입니다.!
-        // 안녕하세요. 20살 홍길동입니다.!
-        // 안녕하세요. 20살 홍길동입니다.!
-        // 안녕하세요. 20살 홍길동입니다.!
-        // 안녕하세요. 20살 홍길동입니다.!
+        greeting2(1, 3);
+        // 안녕하세요
+        // 안녕하세요
+        // 안녕하세요
 
-        introduce(10);
-        // 안녕하세요. 20살 홍길동입니다.!
-        // 안녕하세요. 20살 홍길동입니다.!
-        // 안녕하세요. 20살 홍길동입니다.!
-        // 안녕하세요. 20살 홍길동입니다.!
-        // 안녕하세요. 20살 홍길동입니다.!
-        // 안녕하세요. 20살 홍길동입니다.!
-        // 안녕하세요. 20살 홍길동입니다.!
-        // 안녕하세요. 20살 홍길동입니다.!
-        // 안녕하세요. 20살 홍길동입니다.!
-        // 안녕하세요. 20살 홍길동입니다.!
+        greeting2(2, 5);
+        // 하이~
+        // 하이~
+        // 하이~
+        // 하이~
+        // 하이~
+
+        greeting2(3, 7);
+        // 봉쥬
+        // 봉쥬
+        // 봉쥬
+        // 봉쥬
+        // 봉쥬
+        // 봉쥬
+        // 봉쥬
     }
 
 }
