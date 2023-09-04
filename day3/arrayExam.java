f5bf006185a4cc2d1c27bd482b2a78f971d04502
habinnn <gkqls7423@gmail.com>
2023-09-04 14:47:49 +0900

    4일차

--- day3/arrayExam.java
+++ day3/arrayExam.java
@@ -1,54 +1,10 @@
 package day3;
 public class arrayExam {
     // 배열을 두번 출력해주세요.
-    public static void main(String[] args) {
-        int[][] darr = {
-                {1, 2, 3, 4},
-                {5, 6, 7, 8},
-                {9,10,11,12},
-                {13,14,15,16}
-        };
+    public static double circleArea(int radius) { // void -> double로 수정. 리턴타입은 리턴값과 자료형이 일치해야함.
+        double result = radius * radius * 3.14;
+        System.out.println(result);
 
-//        for(int i = 0; i < 4; i++){
-//            for(int j =0; j < 4; j++){
-//                System.out.println(darr[i][j]);
-//            }
-//        }
-
-
-
-        for(int i = 3; i > -1 ; i--){
-            for(int j = 3 ; j >= 0 ; j--)
-                System.out.println(darr[j][i]);
-        }
-
-/*
-* 1 2 3 4
-* 5 6 7 8
-* 9 10 11 12
-* 13 14 15 16
-*
-* */
-
-
-        // 1. 다음과 같이 출력되게 해주세요.
-    /*
-
-    4 3 2 1
-    8 7 6 5
-    12 11 10 9
-    16 15 14 13
-
-    */
-
-        // 2. 다음과 같이 출력되게 해주세요.
-    /*
-
-    16 12 8 4
-    15 11 7 3
-    14 10 6 2
-    13 9  5  1
-
-    */
+        return result; // 함수에서 리턴할 때 result값을 들고감.
     }
 }
---
+++ day4/ex.java
@@ -0,0 +1,21 @@
+//package day4;
+//
+//public class ex {
+//
+//    public static class Car{
+//        String speed1;
+//        String speed2;
+//    }
+//    public static void main(String[] args) {
+//
+//        Car car = new Car();
+//        car.speed1 = "자동차가 최대속력 220km로 달립니다.";
+//        car.speed2 = "자동차가 최대속력 250km로 달립니다.";
+//        System.out.println(car.speed1);
+//        System.out.println(car.speed2);
+//
+//
+//        // 출력 : 자동차가 최대속력 220km로 달립니다.
+//        // 출력 : 자동차가 최대속력 250km로 달립니다.
+//    }
+//}
