7d853fc94493c65746645826500d7c1506ac68aa
habinnn <gkqls7423@gmail.com>
2023-09-11 17:45:08 +0900

    시험

--- test/Exam2.java
+++ test/Exam2.java
@@ -4,69 +4,76 @@ public class Exam2 {
 
     public static void main(String[] args) {
 
-        for(int 세로 = 1; 세로 < 3; 세로++){
-            if(세로 > 0) {
-                System.out.println(" ");
-            }
-           for(int 가로 = 0; 가로 < 5; 가로 ++){
-               System.out.printf("*");
+        System.out.println("  *");
+        System.out.println(" ***");
+        System.out.println("*****");
 
-           }
-        }
+        System.out.println("   *");
+        System.out.println("  ***");
+        System.out.println(" *****");
+        System.out.println("*******");
+
+        System.out.println("     *");
+        System.out.println("    ***");
+        System.out.println("   *****");
+        System.out.println("  *******");
+        System.out.println(" *********");
+        System.out.println("***********");
+
 
 
 
 
         // 높이를 입력해주세요 : 3                // 출력
 
-        /*
+    /*
 
-           *
-          ***
-         *****
+      *
+     ***
+    *****
 
-         */
+     */
 
-        // 높이를 입력해주세요 : 5
+    // 높이를 입력해주세요 : 5
 
-        // 출력
+    // 출력
 
-        /*
+    /*
 
-              *
+     *
 
-             ***
+     ***
 
-            *****
+     *****
 
-           *******
+     *******
 
-          *********
+     *********
 
-         */
+     */
 
-        // 높이를 입력해주세요 : 7
+    // 높이를 입력해주세요 : 7
 
-        // 출력
+    // 출력
 
-        /*
+    /*
 
-                  *
+     *
 
-                 ***
+     ***
 
-                *****
+     *****
 
-               *******
+     *******
 
-              *********
+     *********
 
-             ***********
+     ***********
 
-            *************
+     *************
 
-         */
+     */
 
-    }
+}
 
 }
